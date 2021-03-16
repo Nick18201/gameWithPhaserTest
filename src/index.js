92ab9dbb2177849f3df54beffcdeae18d19e1542
nicolas blum <nblumferracci@gmail.com>
2021-03-16 13:16:07 +0100

    adding bomb and score count

--- src/index.js
+++ src/index.js
@@ -5,77 +5,170 @@ let config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-        gravity: { y: 300 },
-        debug: false
-    }
-},
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
-      preload: preload,
-      create: create,
-      update: update
-  }
+    preload: preload,
+    create: create,
+    update: update,
+  },
 };
+let player;
+let platforms;
+let stars;
+let bombs;
+let cursors;
+let score = 0;
+let scoreText;
+let gameOver = false;
 
 let game = new Phaser.Game(config);
 
-function preload ()
-{
-  this.load.image('sky', 'src/assets/skyalt.png');
-  this.load.image('platform', 'src/assets/platform.png');
-  this.load.image('star', 'src/assets/star.png');
-  this.load.image('bomb', 'src/assets/bomb.png');
-  this.load.spritesheet('dude', 
-      'src/assets/dude.png',
-      { frameWidth: 32, frameHeight: 48 }
-  );
+function preload() {
+  this.load.image("sky", "src/assets/skyalt.png");
+  this.load.image("platform", "src/assets/platform.png");
+  this.load.image("star", "src/assets/star.png");
+  this.load.image("bomb", "src/assets/bomb.png");
+  this.load.spritesheet("dude", "src/assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
 }
 
-let platforms;
-let player;
-
-function create ()
-{
-  this.add.image(0, 0, 'sky').setOrigin(0, 0); //setOrigin : 0,0 = haut à gauche / 0.5,0.5 = centre / 1,1 = bas à droite
+function create() {
+  this.add.image(0, 0, "sky").setOrigin(0, 0); //setOrigin : 0,0 = haut à gauche / 0.5,0.5 = centre / 1,1 = bas à droite
   // this.add.image(400, 300, 'star');
 
+  // Adding platforms with physics
   platforms = this.physics.add.staticGroup();
 
-  platforms.create(400, 568, 'platform').setScale(2.5).refreshBody(); 
-  // The call to refreshBody() is required because we have scaled a static physics body, 
+  platforms.create(400, 568, "platform").setScale(2.5).refreshBody();
+  // The call to refreshBody() is required because we have scaled a static physics body,
   // so we have to tell the physics world about the changes we made.
 
-  platforms.create(600, 400, 'platform');
-  platforms.create(50, 250, 'platform');
-  platforms.create(750, 220, 'platform');
+  platforms.create(600, 400, "platform");
+  platforms.create(50, 250, "platform");
+  platforms.create(750, 220, "platform");
 
-  player = this.physics.add.sprite(100, 450, 'dude');
+  // Creating a character
+  player = this.physics.add.sprite(100, 450, "dude");
 
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
+  player.body.setGravityY(300);
 
+  // Adding basics animations
   this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
     frameRate: 10,
-    repeat: -1
+    repeat: -1,
   });
 
   this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
   });
 
   this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
     frameRate: 10,
-    repeat: -1
+    repeat: -1,
   });
+
+  // Adding Keyboard management system (doesn't require event listener)
+  cursors = this.input.keyboard.createCursorKeys();
+
+  // Adding stars element to give the game a purpose
+  stars = this.physics.add.group({
+    key: "star",
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 },
+  });
+
+  stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+
+  // Adding some bombs enemy with physic
+  bombs = this.physics.add.group();
+  // Setting score display
+  scoreText = this.add.text(16, 16, "score: 0", {
+    fontSize: "32px",
+    fill: "#000",
+  });
+
+  // Adding collision between player platforms
+  this.physics.add.collider(player, platforms);
+  // Adding collision between stars and platforms
+  this.physics.add.collider(stars, platforms);
+  // Adding collision between bombs and platforms
+  this.physics.add.collider(bombs, platforms);
+
+  // Adding collision overlap between player and stars and player and bombs
+  this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
-function update ()
-{
+function update() {
+  if (gameOver) {
+    return;
+  }
+  // Populating the cursor object with properties
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play("turn");
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-530);
+  }
+}
+
+function collectStar(player, star) {
+  // allow stars to disappear when player hit them
+  star.disableBody(true, true);
+  // manipulating score
+  score += 10;
+  scoreText.setText("Score: " + score);
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    let x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+
+    let bomb = bombs.create(x, 16, "bomb");
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  }
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+
+  player.setTint(0xff0000);
+
+  player.anims.play("turn");
+
+  gameOver = true;
 }
