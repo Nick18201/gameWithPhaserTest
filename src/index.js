9d571f9dae630c5dfb3db0c9cef301e6e79aa159
Guillaume Azzopardi <guillaume.dambraazzopardi@gmail.com>
2021-03-16 10:32:07 +0100

    Adding character and modified background

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ let game = new Phaser.Game(config);
 
 function preload ()
 {
-  this.load.image('sky', 'src/assets/sky.png');
-  this.load.image('ground', 'src/assets/platform.png');
+  this.load.image('sky', 'src/assets/skyalt.png');
+  this.load.image('platform', 'src/assets/platform.png');
   this.load.image('star', 'src/assets/star.png');
   this.load.image('bomb', 'src/assets/bomb.png');
   this.load.spritesheet('dude', 
@@ -33,6 +33,7 @@ function preload ()
 }
 
 let platforms;
+let player;
 
 function create ()
 {
@@ -41,13 +42,38 @@ function create ()
 
   platforms = this.physics.add.staticGroup();
 
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody(); 
+  platforms.create(400, 568, 'platform').setScale(2.5).refreshBody(); 
   // The call to refreshBody() is required because we have scaled a static physics body, 
   // so we have to tell the physics world about the changes we made.
 
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
+  platforms.create(600, 400, 'platform');
+  platforms.create(50, 250, 'platform');
+  platforms.create(750, 220, 'platform');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
 }
 
 function update ()
